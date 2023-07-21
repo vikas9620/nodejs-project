1f5cf19a79501b346618f2f34c4f4d1e0a605474
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2023-07-21 21:31:37 +0530

    Request / Response

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('VIKAS SINGH');
+  const { url } = req;
+if(url === '/') {
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+  res.write('</html>');
+  return res.end();
+}else if (url === '/home') {
+  res.write('<html>');
+  res.write('<head><title>home</title></head>');
+  res.write('<body><h1>Welcome home</h1></body>');
+  res.write('</html>');
+  return res.end();
+  } else if (url === '/about') {
+    res.write('<html>');
+  res.write('<head><title>about us</title></head>');
+  res.write('<body><h1>Welcome to About Us page</h1></body>');
+  res.write('</html>');
+    return res.end();
+  } else if (url === '/node') {
+    res.write('<html>');
+  res.write('<head><title>node</title></head>');
+  res.write('<body><h1>Welcome to my Node.js project</h1></body>');
+  res.write('</html>');
+    return res.end();
+  } else {
+    res.end('Page not found');
+  }
 });
 
-server.listen(4000);
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
