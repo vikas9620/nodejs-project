f5a2b529dada7ead8029d7e3023f85cbfa67b6a1
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2023-07-24 21:13:38 +0530

    Nodemon and Package JSON

--- app.js
+++ app.js
@@ -44,5 +44,5 @@ const server = http.createServer(routes);
 const port = 3000;
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running ${port}`);
 });
