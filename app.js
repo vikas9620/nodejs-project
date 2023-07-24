b945557cfe0bd0cb083eda6795955f73a11ded40
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2023-07-24 16:26:47 +0530

    Clean Up Code

--- app.js
+++ app.js
@@ -37,57 +37,9 @@ server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });*/
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    
-    fs.readFile('form_data.txt', 'utf8', (err, data) => {
-      let messages = [];
-      if (!err && data) {
-        messages = data.trim().split('\n');
-      }
-
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(`
-        <h2>Messages:</h2>
-        <ul>
-          ${messages[messages.length - 1]}
-        </ul>
-        <form method="post" action="/">
-          <label for="name">Name:</label>
-          <input type="text" id="name" name="name">
-          <input type="submit" value="Submit">
-        </form>
-      `);
-    });
-  } else if (req.method === 'POST') {
-
-    let data = '';
-    req.on('data', chunk => {
-      data += chunk;
-    });
-
-    req.on('end', () => {
-      const formData = new URLSearchParams(data);
-      const name = formData.get('name');
-
-      fs.appendFile('form_data.txt', `${name}\n`, err => {
-        if (err) {
-          console.error('Error writing to file:', err);
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal Server Error');
-        } else {
-          res.writeHead(302, { 'Location': '/' });
-          res.end();
-        }
-      });
-    });
-  } else {
-    res.writeHead(405, { 'Content-Type': 'text/plain' });
-    res.end('Method Not Allowed');
-  }
-});
+const routes = require('./router');
+const server = http.createServer(routes);
 
 const port = 3000;
 
