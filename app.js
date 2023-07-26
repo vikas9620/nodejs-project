eb99e0df9daa155b7b6233864f122c00e71d9f54
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2023-07-26 22:29:59 +0530

    Intro to Middlewares

--- app.js
+++ app.js
@@ -37,15 +37,22 @@ server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });*/
 
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  console.log("in the middleware")
-  next();
-})
-app.use((req, res, next) => {
-  console.log("in another middleware")
-  res.send('<h1>Hello from express</h1>');
-})
-app.listen(3000)
+app.use('/add-product',(req, res, next) => {
+  console.log("in another middleware");
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+});
+app.post('/product',(req, res, next) => {
+console.log(req.body);
+res.redirect('/');
+
+})  
+app.use('/',(req, res, next) => {
+  console.log("in another middleware");
+  res.send("<h1>Hello from express</h1>");
+});
+app.listen(3000);
