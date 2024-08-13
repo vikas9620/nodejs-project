8b51a6a2c7c64b6b2ba3d52be1259c99f17f3c97
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2024-08-13 15:35:41 +0530

    Retrieving data from databases

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,8 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect("/");
+  product.save().then(()=>{res.redirect("/");}).catch((err) => {console.log(err)});
+ 
 };
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
@@ -53,9 +53,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
+  Product.fetchAll().then(([rows]) => {
     res.render("admin/products", {
-      prods: products,
+      prods: rows,
       pageTitle: "Admin Products",
       path: "/admin/products",
     });
